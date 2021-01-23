189e00d946d7cb57eeb8e9ab5e036980cdc7302c
Neven Boric <nboric@gmail.com>
2021-01-22 21:14:59 -0800

    Start with enemy hit point logic

--- src/scripts/enemy.ts
+++ src/scripts/enemy.ts
@@ -1,8 +1,20 @@
 import {Direction, GameElement, Pos, HitRegion, Collisionable} from "./types.js";
 import {drawCircle} from "./util.js";
 
+enum Status {
+    NORMAL,
+    HIT,
+    DYING,
+    DEAD
+}
+
 export interface Enemy extends GameElement{
     isEnemy: boolean
+    hitPoints: number
+    status: Status
+    elapsed: number
+
+    shouldDisappear(): boolean
 }
 
 export function isEnemy(el: Collisionable): el is Enemy{
@@ -16,29 +28,80 @@ class SimpleEnemy implements Enemy
     speed: number
     isEnemy: boolean
     isHit: boolean
+    hitPoints: number
+    status: Status
+    elapsed: number
 
     constructor(pos: Pos) {
         this.pos = {x: pos.x, y: pos.y}
 
         this.dir = {x: -1 + Math.random() * 2, y: -1 + Math.random() * 2}
 
-        this.speed = 1 + Math.random() * 2
+        this.speed = 1 + Math.random() * 1.5
 
         this.isEnemy = true
+
+        this.hitPoints = 3
+        this.status = Status.NORMAL
+        this.elapsed = 0
     }
 
     render(context: CanvasRenderingContext2D): HitRegion {
-        return {points: drawCircle(context, this.pos, 10, 'rgb(0,255,255)'), element: this}
+        let fill = 'rgb(0,255,255)'
+        switch (this.status){
+            case Status.HIT:
+                fill = 'rgb(90,174,174)'
+                break
+            case Status.DYING:
+                fill = 'rgb(0,34,34)'
+                break
+            default:
+
+        }
+        return {points: drawCircle(context, this.pos, 10, fill), element: this}
     }
 
     update(): void {
+
+        this.elapsed++;
+        switch (this.status)
+        {
+            case Status.DYING:
+                if (this.elapsed > 60 * 2)
+                {
+                    this.status = Status.DEAD
+                }
+                break
+            case Status.HIT:
+                if (this.elapsed > 60 * 1)
+                {
+                    this.status = Status.NORMAL
+                }
+                break
+            default:
+        }
 
         this.pos.x += this.dir.x * this.speed
         this.pos.y += this.dir.y * this.speed
     }
 
     hit(): boolean {
+        if (~this.isHit) {
+            if (this.status == Status.NORMAL)
+            {
+                this.elapsed = 0
+                this.status = Status.HIT
+                this.hitPoints--
+                if (this.hitPoints == 0){
+                    this.status = Status.DYING
+                }
+            }
+        }
         return true
+    }
+
+    shouldDisappear(): boolean {
+        return this.status == Status.DEAD
     }
 
 }
@@ -67,5 +130,6 @@ export class EnemyManager
 
     updateAll(): void {
         this.enemies.forEach(enemy => enemy.update())
+        this.enemies = this.enemies.filter(e => !e.shouldDisappear())
     }
 }
